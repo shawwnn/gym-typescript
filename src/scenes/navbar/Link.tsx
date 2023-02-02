0a936b2344eb9a85553550a063b2f2fd39347c9b
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-02-03 07:48:52 +0800

    navbar

---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,30 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+
+
+type Props = {
+	page: string;
+	selectedPage: string;
+	setSelectedPage: (value: string) => void;
+}
+
+const Link = ({
+	page,
+	selectedPage,
+	setSelectedPage,
+}: Props) => {
+	const lowerCasePage = page.toLowerCase().replace(/ /g, "") //regex removing space
+
+  return (
+    <AnchorLink
+			href={`#${lowerCasePage}`}
+			className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+				transition duration-500 hover:text-primary-300
+			`}
+			onClick={() => setSelectedPage(lowerCasePage)}
+		>
+      {page}
+    </AnchorLink>
+  )
+}
+
+export default Link
